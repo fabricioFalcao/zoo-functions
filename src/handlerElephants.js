e9be678c8eeefbef0c5ca58945db15acbe1c796a
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-19 12:06:12 -0300

    Requisitos 7 e 15

--- src/handlerElephants.js
+++ src/handlerElephants.js
@@ -35,4 +35,4 @@ const handlerElephants = (param) => {
 
 module.exports = handlerElephants;
 
-console.log(handlerElephants('averageAge'));
+console.log(handlerElephants('availability'));
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -19,4 +19,13 @@ describe('Testes da função HandlerElephants', () => {
   test('A função so ser chamada com o parâmetro "averageAge", retorna "10.5"', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
   });
+  test('A função so ser chamada com o parâmetro "location", retorna "NW"', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  test('A função so ser chamada com o parâmetro "popularity", retorna "5"', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  test('A função so ser chamada com o parâmetro "availability", retorna os dias disponíves para visitação', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
 });
