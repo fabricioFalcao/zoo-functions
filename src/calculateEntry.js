3e21ebcab0110aa85e8d17dd91f37bdab6ff9d25
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-19 13:00:27 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
+const countEntrants = (entrants = []) => {
+  const visitors = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      visitors.child += 1;
+    }
+    if (person.age >= 18 && person.age < 50) {
+      visitors.adult += 1;
+    }
+    if (person.age >= 50) {
+      visitors.senior += 1;
+    }
+  });
+  return visitors;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  const visitors = countEntrants(entrants);
+  const { adult, senior, child } = data.prices;
+
+  return visitors.adult * adult + visitors.senior * senior + visitors.child * child;
 };
 
 module.exports = { calculateEntry, countEntrants };
