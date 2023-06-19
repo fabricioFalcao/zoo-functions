4d2493e80cc416b61f72f3c3dc2d8239a03ba65d
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-19 11:58:53 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('A função ao ser chamada sem receber um parâmetro retorna "undefined"', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  test('A função so ser chamada com um parâmetro diferente de uma string, retorna a mensagem de erro', () => {
+    expect(handlerElephants(30)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  test('A função so ser chamada com um parâmetro inexperado, retorna "null"', () => {
+    expect(handlerElephants('diet')).toBeNull();
+  });
+  test('A função so ser chamada com o parâmetro "count", retorna "4"', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('A função so ser chamada com o parâmetro "anmes", retorna os nomes dos elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  test('A função so ser chamada com o parâmetro "averageAge", retorna "10.5"', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+});
