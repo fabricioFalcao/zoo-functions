2f11460af37ef4c85fdea7e2c4ccc7fada14d3bb
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-19 10:43:37 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const animalsObject = {};
+  if (!animal) {
+    data.species
+      .forEach((specie) => {
+        animalsObject[specie.name] = specie.residents.length;
+      });
+    return animalsObject;
+  }
+  return data.species
+    .find(({ name }) => name === animal.species)
+    .residents
+    .filter(({ sex }) => (animal.sex ? sex === animal.sex : sex))
+    .length;
 };
 
 module.exports = countAnimals;
