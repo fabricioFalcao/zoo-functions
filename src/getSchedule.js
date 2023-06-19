2e0748a24a389ab432d2bf77b27e572b7fcc400a
fabricioFalcao <fabriciohfd@gmail.com>
2023-06-19 16:33:29 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
+const scheduleByAnimal = (animal) => data.species
+  .find(({ name }) => name === animal)
+  .availability;
+
+const scheduleByDay = (...days) => {
+  const schedule = {};
+  days.flat().forEach((day) => {
+    if (day !== 'Monday') {
+      schedule[day] = {
+        officeHour: `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`,
+        exhibition: data.species
+          .filter(({ availability }) => availability.includes(day))
+          .map(({ name }) => name),
+      };
+    }
+    if (day === 'Monday') {
+      schedule[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return schedule;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const weekDays = Object.keys(data.hours);
+  const zooAnimals = data.species.map(({ name }) => name);
+
+  if (zooAnimals.includes(scheduleTarget)) {
+    return scheduleByAnimal(scheduleTarget);
+  }
+  if (weekDays.includes(scheduleTarget)) {
+    return scheduleByDay(scheduleTarget);
+  }
+  return scheduleByDay(weekDays);
 };
 
 module.exports = getSchedule;
